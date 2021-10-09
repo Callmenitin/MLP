3f2d3e2bf5a082ab60c76811d6f6616f120ff14f
Callmenitin <nklucky1995@gmail.com>
2021-10-09 19:19:34 +0530

    feed forward implemented

--- src/MLP.c
+++ src/MLP.c
@@ -7,17 +7,18 @@
 double **inputs,*outputs;
 double learningRate;
 double **hidden,**weights;
-int rows,start,end,output;
+int rows,_start,_end,output;
 int isNumerical=-1;
 struct OutputMap *mappings;
 int _mapInt=0;
 int _input;
 
 double sigmoid(double x){
-return 1/(1+exp(-x));
+
+return 1.0/(1.0+exp(-x));
 }
 
-double tanh(double x){
+double mytanh(double x){
 return tanh(x);
 }
 
@@ -29,12 +30,12 @@ return x;
 
 void setDataSetParameters(int r,int s,int e,int o){
 rows=r;
-start=s;
-end=e;
+_start=s;
+_end=e;
 output=o;    
 }
 
-void feedForward(struct Classifier obj,struct Neuron inputNeurons[_input],struct Neuron hiddenNeurons[][_input]);
+void feedForward(struct Classifier obj,struct Neuron inputNeurons[_input],struct Neuron hiddenNeurons[][_input],int a,int b);
 
 void train(struct Classifier obj){
 
@@ -60,7 +61,6 @@ hiddenNeurons[i][j].weights[k]=(rand()%10);
 }
 }
 
-
 for(int j=0;j<obj.inputNodes;j++)
 {
 hiddenNeurons[obj.hiddenLayers-1][j].weights[0]=(rand()%10);
@@ -70,7 +70,7 @@ inputs=(double **)malloc(rows*sizeof(double*));
 
 for(int i=0;i<rows;i++)
 {
-inputs[i] =(double *)malloc(end*sizeof(double));
+inputs[i] =(double *)malloc(_end*sizeof(double));
 }
 
 mappings=(struct OutputMap*)malloc(rows*sizeof(struct OutputMap));
@@ -78,16 +78,41 @@ outputs=(double *)malloc(rows*sizeof(double));
 
 readCSV(obj.fileName);
 
+int batchSize,rowsPerBatch;
+
+if(obj.backPropagation==BATCH)
+{
+batchSize=1;
+rowsPerBatch=rows;
+}
+else if (obj.backPropagation==STOCHASTIC){
+batchSize=rows;
+rowsPerBatch=1;
+}
+else{
+batchSize=5;
+rowsPerBatch=rows/5;
+}
 
 
+for(int i=0;i<obj.iterations;i++)
+{
 
-
-
-
-//feedForward(obj,inputNeurons,hiddenNeurons);
+for(int j=0;j<batchSize;j++)
+{
+    int start=j*batchSize;
+    int end=start+rowsPerBatch;
+    feedForward(obj,inputNeurons,hiddenNeurons,start,end);
 
 }
 
+}
+
+for(int i=0;i<rows;i++){
+    printf("\nOutput for row:%d is %.2f",i,outputs[i]);
+}
+
+}
 
 void readCSV(char *fileName){
 
@@ -103,9 +128,9 @@ for(int i=0;i<rows;i++)
 {
 fgets(line, 1024, filePointer); 
 char* tok = strtok(line, ",");
-for(int j=0;j<end;j++)
+for(int j=0;j<_end;j++)
 {
-if(j>=start)
+if(j>=_start)
 {
 
 if(j==output)
@@ -130,7 +155,6 @@ tok = strtok(NULL,",");
 }
 free(line);
  
-
 }
 
 int isNumber(char *str){
@@ -189,11 +213,130 @@ double crossEntropy(double x){
     return -1*log(x);
 }
 
-void feedForward(struct Classifier obj,struct Neuron inputNeurons[_input],struct Neuron outputNeurons[][_input])
+void feedForward(struct Classifier obj,struct Neuron inputNeurons[_input],struct Neuron hiddenNeurons[][_input],int start,int end)
 {
 
+for(int i=start;i<end;i++)
+{
+    int _itr_start,_itr_end;
 
+if(output==_start)
+{_itr_start=output+1; _itr_end=_end;}
+else
+{_itr_start=start; _itr_end=_end-1; }  
+
+//From input Layer to first hidden layer
+for(int k=0;k<obj.inputNodes;k++)
+{
+
+hiddenNeurons[0][k].value=0.0;
+int index=0;
+
+for(int ptr=_itr_start;ptr<_itr_end;ptr++)
+{
+
+hiddenNeurons[0][k].value+=inputs[i][ptr]*inputNeurons[index].weights[k];
+++index; 
+}
+
+}
+//from 2nd Hidden layer to last hidden Layer
+
+for(int k=1;k<obj.hiddenLayers;k++)
+{
+
+int previous=k-1;
+for(int _neuron=0;_neuron<obj.inputNodes;_neuron++)
+{
+hiddenNeurons[k][_neuron].value=0.0;
+for(int ptr=0;ptr<obj.inputNodes;ptr++)
+{
+
+double _value;
+if(obj.activationFunction==SIGMOID){
+_value=sigmoid(hiddenNeurons[previous][ptr].value);
+}
+else if(obj.activationFunction==TANH){
+_value=mytanh(hiddenNeurons[previous][ptr].value);
+
+}
+else{
+_value=relu(hiddenNeurons[previous][ptr].value);
+}
+
+hiddenNeurons[k][_neuron].value+=_value*hiddenNeurons[previous][ptr].weights[_neuron];
+}
 
 }
 
+}
 
+//From last hidden layer to output layer
+
+double _output_value=0.0;
+
+for(int ii=0;ii<obj.inputNodes;ii++){
+    double _value;
+if(obj.activationFunction==SIGMOID){
+
+_value=sigmoid(hiddenNeurons[obj.hiddenLayers-1][ii].value);
+}
+else if(obj.activationFunction==TANH){
+_value=mytanh(hiddenNeurons[obj.hiddenLayers-1][ii].value);
+
+}
+else{
+_value=relu(hiddenNeurons[obj.hiddenLayers-1][ii].value);
+}
+_output_value+=_value*hiddenNeurons[obj.hiddenLayers-1][ii].weights[0];
+
+
+}
+outputs[i]=_output_value;
+
+}
+    
+}              
+
+
+
+
+
+
+/*
+for(int i=0;i<obj.inputNodes;i++)
+{
+    printf("Input Neuron:%d\n",i);
+for(int j=0;j<obj.inputNodes;j++)
+{
+printf(" %.2f",inputNeurons[i].weights[j]);
+
+}
+printf("\n");
+}
+
+
+for(int i=0;i<obj.hiddenLayers-1;i++)
+{
+    printf("Weight matrix for hidden layer:%d\n",i);
+for(int j=0;j<obj.inputNodes;j++)
+{
+for(int k=0;k<obj.inputNodes;k++) 
+printf("%.2f ",hiddenNeurons[i][j].weights[k]);
+
+printf("\n");
+}
+printf("\n");
+}
+
+
+printf("Status of hidden layers and its neurons\n");
+
+for(int ii=0;ii<obj.hiddenLayers;ii++){
+printf("Hidden layer:%d\n",ii);
+for(int jj=0;jj<obj.inputNodes;jj++){
+printf("Neuron:%d, Value=%.2f\n",jj,hiddenNeurons[ii][jj].value);
+
+}}
+printf("\n");
+*/
