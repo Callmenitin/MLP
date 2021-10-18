0714733bf6c9df3be202cc9bd012b550327c0633
Callmenitin <nklucky1995@gmail.com>
2021-10-18 22:40:32 +0530

    header file added

--- include/MLP.h
+++ include/MLP.h
@@ -1,14 +1,54 @@
+#define SIGMOID 0
+#define RELU 1
+#define TANH 2
+#define BATCH 0
+#define MINI_BATCH 1
+#define STOCHASTIC 2
+
 struct Classifier
 {
 int inputNodes;
-int hiddenNodes;
 int hiddenLayers;
 int activationFunction;
-int costFunction;
-int backPropgationFunction;
+int backPropagation;
+double learningRate;
+int iterations;
+int batchSize;
+char *fileName;
 };
 
+struct OutputMap
+{
+char *_class;
+int value;
+};
+
+struct Neuron
+{
+double weights[50];
+double value;
+};
+
+struct OutputNeuron
+{
+double net;
+double soft;
+double actual;
+double error_wrt_net;
+};
 
 double sigmoid(double x);
-double tanh(double x);
+double mytanh(double x);
 double relu(double x);
+void train(struct Classifier obj);
+void setDataSetParameters(int rows,int start,int end,int output);
+void readCSV(char *fileName);
+int isNumber(char*);
+int getMappingInteger(char *);
+int getIntOfClass(char *);
+double crossEntropy(struct OutputNeuron[]);
+void test(struct Classifier,char *fileName,int rows,int start,int end,int output);
+void predict(struct Classifier,char *fileName,int rows,int start,int end);
+void classify(struct Classifier,char *fileName,int rows,int start,int end);
+double MSE(double,double);
+
